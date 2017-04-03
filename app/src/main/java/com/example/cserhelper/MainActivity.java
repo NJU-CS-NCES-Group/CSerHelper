ceded63fe163d4883fcb4408b0da02632aeb9b54
zfwang96 <zfwang96@163.com>
2017-04-03 16:07:25 +0800

    登录界面雏形

--- app/src/main/java/com/example/cserhelper/MainActivity.java
+++ app/src/main/java/com/example/cserhelper/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.cserhelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText editText;
+    private EditText editText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        /* TODO:Checkbox */
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                String inputNumber = editText.getText().toString();
+                String inputPassword = editText2.getText().toString();
+                Log.d("MainActivity", inputNumber);
+                Log.d("MainActivity", inputPassword);
+                break;
+            default:
+                break;
+        }
+
     }
 }
