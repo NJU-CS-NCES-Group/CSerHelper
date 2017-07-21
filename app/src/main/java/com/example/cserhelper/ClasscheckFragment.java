c65a30d2c909f3a02cefec4fffd9e92d3c7ac295
zfwang96 <zfwang96@163.com>
2017-07-21 15:38:26 +0800

    add fragments of QR  code scanning and photo taking, file persistence related are remained

--- app/src/main/java/com/example/cserhelper/ClasscheckFragment.java
+++ app/src/main/java/com/example/cserhelper/ClasscheckFragment.java
@@ -1,14 +1,28 @@
 package com.example.cserhelper;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.znq.zbarcode.CaptureActivity;
+
+import java.io.File;
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -18,14 +32,17 @@ import android.widget.Button;
  * Use the {@link ClasscheckFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ClasscheckFragment extends Fragment implements View.OnClickListener{
+public class ClasscheckFragment extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
-
+    //private ImageView picture = (ImageView) View.findViewById(R.id.picture);
+    private Uri imageUri;
+    public static final int TAKE_PHOTO = 1;
+    public static final int QR_CODE = 2;
     public ClasscheckFragment() {
         // Required empty public constructor
     }
@@ -58,18 +75,56 @@ public class ClasscheckFragment extends Fragment implements View.OnClickListener
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_classcheck, container, false);
-        Button button=(Button)view.findViewById(R.id.homeButton);
+        View view = inflater.inflate(R.layout.fragment_classcheck, container, false);
+        Button button = (Button) view.findViewById(R.id.homeButton);
         button.setOnClickListener(this);
+
+        Button scanCode = (Button) view.findViewById(R.id.scan_code);
+        scanCode.setOnClickListener(this);
+
+        Button takePhoto = (Button) view.findViewById(R.id.take_photo);
+
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 启动相机程序
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivityForResult(intent, TAKE_PHOTO);
+            }
+        });
         return view;
     }
 
     @Override
     public void onClick(View v) {
-        int id=v.getId();
-        switch (id){
+        int id = v.getId();
+        switch (id) {
             case R.id.homeButton:
-                startActivity(new Intent(this.getActivity(),PersonalCenterActivity.class));
+                startActivity(new Intent(this.getActivity(), PersonalCenterActivity.class));
+                break;
+            /*case R.id.take_photo:
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                break;*/
+            case R.id.scan_code:
+                    Intent intent = new Intent(this.getActivity(), CaptureActivity.class);
+                    intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
+                    startActivityForResult(intent, QR_CODE);
+                break;
         }
     }
-}
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == QR_CODE) {
+            if (resultCode == RESULT_OK) {
+                //String contents = data.getStringExtra("SCAN_RESULT");
+                //Toast.makeText(this.getContext(), contents + "", Toast.LENGTH_SHORT).show();
+            }
+            if(resultCode == RESULT_CANCELED){
+                //handle cancel
+            }
+        }
+    }
+
+};
